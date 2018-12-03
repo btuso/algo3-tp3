31d96325d700e95aaa7a3e66a02396b3332431b7
Celeste <celeste.rodriguez97@hotmail.com>
2018-12-02 22:12:23 -0300

    ya tengo las estructuras de savings y los calculos

--- src/savings.cc
+++ src/savings.cc
@@ -3,12 +3,54 @@
 #include "vector"
 
 #include "auxiliares.h"
+#include "truck.h"
+
+using namespace	std;
 
 namespace savings {
 
-	std::vector<int> solveCvrp(Point& warehouse, std::vector<Point> &points, int capacity){
-		std::cout << "Entre en savings eeeee\n";
-		return std::vector<int>();
+	vector<int> solveCvrp(Point& warehouse,	vector<Point> &points, int capacity){
+		/* Armo matriz de distancias entre cada par de nodos */
+		vector<vector<float> > distances(points.size(), vector<float>(points.size(), 0));
+		/* Vector de distancia de todo nodo a deposito */
+		vector<float> distance_to_warehouse(points.size(), 0);
+		/* Vector de savings de tamano Nc2 */
+		vector<Saving> savings;
+		savings.reserve(points.size()*(points.size()-1)/2);
+
+		/* Inicializo distancias y calculo savings */
+		calcularDistancias(distances, distance_to_warehouse, points, warehouse);
+		calcularSavings(distances, distance_to_warehouse, points, savings);
+
+		/* Ordeno los savings de mayor a menor */
+		sort(savings.begin(), savings.end());
+		reverse(savings.begin(), savings.end());
+		
+		// cout << "Saving size " << savings.size();
+		// for (int i = 0; i < savings.size(); i++) cout << savings[i].saving << endl;
+
+		vector<Truck> trucks;
+
+		return	vector<int>();
+	}
+
+	void calcularDistancias(vector<vector<float> > &distances, vector<float> &distance_to_warehouse, vector<Point> &points, Point& warehouse){
+		for (unsigned int i = 0; i < points.size(); i++){
+			for (unsigned int j = i+1; j < points.size(); j++){
+				float point_distance = points[i].DistanceTo(points[j]);
+				distances[i][j] = point_distance;
+				distances[j][i] = point_distance;
+			}
+		}
+		for (unsigned int i = 0; i < points.size(); i++)
+			distance_to_warehouse[i] = points[i].DistanceTo(warehouse);
+	}
+	void calcularSavings(vector<vector<float> > &distances, vector<float> &distance_to_warehouse, vector<Point> &points, vector<Saving> &savings){
+		for(unsigned int i = 0; i < points.size(); i++){
+			for (unsigned int j = i+1; j < points.size(); j++){
+				savings.push_back(Saving(distances, distance_to_warehouse, i, j));
+			}
+		}
 	}
 
 }
--- src/savings.h
+++ src/savings.h
@@ -4,11 +4,16 @@
 #include "vector"
 
 #include "point.h"
+#include "saving_struct.h"
 
 namespace savings {
 
 	std::vector<int> solveCvrp(Point& warehouse, std::vector<Point> &points, int capacity);
+	void calcularDistancias(std::vector<std::vector<float> > &distances, std::vector<float> &distance_to_warehouse, std::vector<Point> &points, Point& warehouse);
+	void calcularSavings(std::vector<std::vector<float> > &distances, std::vector<float> &distance_to_warehouse, std::vector<Point> &points, std::vector<Saving> &savings);
 
-}
+
+};
+
 
 #endif
